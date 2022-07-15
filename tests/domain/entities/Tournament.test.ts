6faf9c785a9a49cbafc24a082e4e14989c2e8349
lucas6g <lucas.trabalho1533@gmail.com>
2022-07-15 17:50:03 -0300

    test: ensure create Tournament instance with a game

--- tests/domain/entities/Tournament.test.ts
+++ tests/domain/entities/Tournament.test.ts
@@ -1,12 +1,23 @@
+import Game from '@/domain/entities/Game'
 import { Tournament } from '@/domain/entities/Tournament'
 
 describe('Tournament', () => {
   it('should create a Tournament', () => {
     const startAt = new Date('2022-07-15T10:00:00')
     const endsAt = new Date('2022-07-15T12:00:00')
+    const game = new Game('anyGameId', 'anyGameName', 'anyGameImage', 'anyGameType', 'anyGameMode')
 
-    const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)
+    const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5, game)
 
     expect(sut.getPrize()).toBe(0)
+  })
+  it('should create a Tournament with a game', () => {
+    const startAt = new Date('2022-07-15T10:00:00')
+    const endsAt = new Date('2022-07-15T12:00:00')
+    const game = new Game('anyGameId', 'anyGameName', 'anyGameImage', 'anyGameType', 'anyGameMode')
+    const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5, game)
+
+    expect(sut.getGame().getName()).toBe('anyGameName')
+    expect(sut.getGame().getImage()).toBe('anyGameImage')
   })
 })
