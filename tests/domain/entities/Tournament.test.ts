6ad9814684752e04a5c25d7343f9ce2cacff39c7
lucas6g <lucas.trabalho1533@gmail.com>
2022-07-16 18:18:36 -0300

    test: ensure not create a Tournament without a game

--- tests/domain/entities/Tournament.test.ts
+++ tests/domain/entities/Tournament.test.ts
@@ -1,18 +1,22 @@
 
+import { Game } from '@/domain/entities/Game'
 import { Tournament } from '@/domain/entities/Tournament'
 import { InvalidPreviosDateError } from '@/domain/errors/InvalidPreviosDateError'
 
 describe('Tournament', () => {
-  it('should create a Tournament', () => {
+  it('should create a Tournament with a game', () => {
     const startAt = new Date('2022-07-15T17:00:00')
     const endsAt = new Date('2022-07-15T19:00:00')
 
     jest.spyOn(Date, 'now').mockImplementation(() => {
       return new Date('2022-07-15T14:00:00').getTime()
     })
-    const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)
+
+    const game = new Game('anyGameId', 'anyGameName', 'anyGameType', 'anyGameImage', 'anyGameMode')
+    const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5, game)
 
     expect(sut.getPrize()).toBe(0)
+    expect(sut.getGame()).toBeInstanceOf(Game)
   })
 
   it('should not create a Tournament whit invalid startAt date', () => {
@@ -22,7 +26,9 @@ describe('Tournament', () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date('2022-07-15T14:00:00').getTime()
     })
-    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
+    const game = new Game('anyGameId', 'anyGameName', 'anyGameType', 'anyGameImage', 'anyGameMode')
+
+    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5, game)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
   })
 
   it('should not create a Tournament with invalid endsAt date', () => {
@@ -32,6 +38,7 @@ describe('Tournament', () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date('2022-07-15T14:00:00').getTime()
     })
-    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
+    const game = new Game('anyGameId', 'anyGameName', 'anyGameType', 'anyGameImage', 'anyGameMode')
+    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5, game)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
   })
 })
