121769f02aaa0bf53842dfe353fd0e06f8eb671e
lucas6g <lucas.trabalho1533@gmail.com>
2022-07-16 17:37:41 -0300

    test: ensure not create a Tournament with past endAt date

--- tests/domain/entities/Tournament.test.ts
+++ tests/domain/entities/Tournament.test.ts
@@ -1,14 +1,14 @@
 
 import { Tournament } from '@/domain/entities/Tournament'
-import { InvalidDateError } from '@/domain/errors/InvalidStartAtDateAtError'
+import { InvalidPreviosDateError } from '@/domain/errors/InvalidPreviosDateError'
 
 describe('Tournament', () => {
   it('should create a Tournament', () => {
-    const startAt = new Date('2022-07-15T15:00:00')
-    const endsAt = new Date('2022-07-15T17:00:00')
+    const startAt = new Date('2022-07-15T17:00:00')
+    const endsAt = new Date('2022-07-15T19:00:00')
 
-    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date('2022-07-15T12:00:00').getTime()
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date('2022-07-15T14:00:00').getTime()
     })
     const sut = new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)
 
@@ -20,8 +20,18 @@ describe('Tournament', () => {
     const endsAt = new Date('2022-07-15T17:00:00')
 
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date('2022-07-15T12:00:00').getTime()
+      return new Date('2022-07-15T14:00:00').getTime()
     })
-    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)).toThrow(new InvalidDateError('Invalid date'))
+    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
+  })
+
+  it('should not create a Tournament with invalid endsAt date', () => {
+    const startAt = new Date('2022-07-15T15:00:00')
+    const endsAt = new Date('2022-07-14T17:00:00')
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date('2022-07-15T14:00:00').getTime()
+    })
+    expect(() => new Tournament('anyTournomentId', startAt, endsAt, 5, 1.5)).toThrow(new InvalidPreviosDateError('Invalid previos date'))
   })
 })
