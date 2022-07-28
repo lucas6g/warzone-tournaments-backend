2aa9b99579276af0afab54ab0bf74bfd262c9a14
lucas6g <lucas.trabalho1533@gmail.com>
2022-07-28 14:16:25 -0300

    feat: ensure return all players inside a Team

--- src/domain/entities/Team.ts
+++ src/domain/entities/Team.ts
@@ -1,5 +1,4 @@
 import { Player } from '@/domain/entities/Player'
-import { PlayerRole } from '@/domain/enums/PlayerRole'
 
 export class Team {
   private readonly id: string
@@ -29,41 +28,7 @@ export class Team {
   }
 
   getPlayers (): Player[] {
-    const player1 = new Player(
-      'anyId',
-      'anyName',
-      'anyEmail',
-      'anyPassword',
-      1.2,
-      'anyPixkey',
-      PlayerRole.COMMON,
-      'anyGamerTag',
-      'anyPlatForm'
-    )
-    const player2 = new Player(
-      'anyId',
-      'anyName',
-      'anyEmail',
-      'anyPassword',
-      1.12,
-      'anyPixkey',
-      PlayerRole.COMMON,
-      'anyGamerTag',
-      'anyPlatForm'
-    )
-    const player3 = new Player(
-      'anyId',
-      'anyName',
-      'anyEmail',
-      'anyPassword',
-      1.11,
-      'anyPixkey',
-      PlayerRole.COMMON,
-      'anyGamerTag',
-      'anyPlatForm'
-    )
-
-    return [player1, player2, player3]
+    return this.players
   }
 
   getPlayerId (): string {
