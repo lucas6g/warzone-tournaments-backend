e2d1e00c0d9d57faea3c62ea6f6555ff9dcc2932
lucas6g <lucas.trabalho1533@gmail.com>
2022-08-01 12:31:02 -0300

    refactor:move sut creation to beforeEach jest method

--- tests/domain/entities/Team.test.ts
+++ tests/domain/entities/Team.test.ts
@@ -2,8 +2,10 @@ import { Player } from '@/domain/entities/Player'
 import { Team } from '@/domain/entities/Team'
 
 describe('Team', () => {
-  it('should create a Team with a leader', () => {
-    const sut = new Team(
+  let sut: Team
+
+  beforeEach(() => {
+    sut = new Team(
       'anyId',
       'anyName',
       'anyLogo',
@@ -18,27 +20,14 @@ describe('Team', () => {
         'anyPlatForm'
       )
     )
+  })
 
+  it('should create a Team with a leader', () => {
     expect(sut.getId()).toBe('anyId')
     expect(sut.getLeaderId()).toBe('leaderId')
     expect(sut.getTotalPlayers()).toBe(1)
   })
   it('should add a Player to the Team', () => {
-    const sut = new Team(
-      'anyId',
-      'anyName',
-      'anyLogo',
-      new Player(
-        'leaderId',
-        'anyName',
-        'anyEmail',
-        'anyPassword',
-        1.2,
-        'anyPixkey',
-        'anyGamerTag',
-        'anyPlatForm'
-      )
-    )
     sut.addPlayer(
       new Player(
         'leaderId',
