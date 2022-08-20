2db3185661df184b27b4250c19b93513ec12a42a
lucas6g <lucas.trabalho1533@gmail.com>
2022-08-20 12:49:38 -0300

    feat:ensure AddGame calls IDGenerator at least once time

--- src/aplication/usecases/AddGame.ts
+++ src/aplication/usecases/AddGame.ts
@@ -1,7 +1,11 @@
+import { IDGenerator } from '@/aplication/protocols/IDGenerator'
 import { UseCase } from '@/aplication/protocols/UseCase'
 
 export class AddGame implements UseCase<Input, Output> {
+  constructor (private readonly idGenerator: IDGenerator) {}
+
   async execute (input: Input): Promise<Output> {
+    this.idGenerator.generate()
     return await Promise.resolve({ status: 'created' })
   }
 }
--- tests/aplication/usecases/AddGame.spec.ts
+++ tests/aplication/usecases/AddGame.spec.ts
@@ -1,8 +1,16 @@
+import { IDGenerator } from '@/aplication/protocols/IDGenerator'
 import { AddGame } from '@/aplication/usecases/AddGame'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('AddGame', () => {
+  let idGenerator: MockProxy<IDGenerator>
+
+  beforeEach(() => {
+    idGenerator = mock<IDGenerator>()
+  })
+
   it('should add a new game', async () => {
-    const sut = new AddGame()
+    const sut = new AddGame(idGenerator)
 
     const input = {
       name: 'warzone'
@@ -10,5 +18,15 @@ describe('AddGame', () => {
 
     const output = await sut.execute(input)
     expect(output.status).toBe('created')
+  })
+  it('should call id generator', async () => {
+    const sut = new AddGame(idGenerator)
+
+    const input = {
+      name: 'warzone'
+    }
+
+    await sut.execute(input)
+    expect(idGenerator.generate).toHaveBeenCalledTimes(1)
   })
 })
