c165b1c2d2f8bd555e125f44518c3431bd071256
lucas6g <lucas.trabalho1533@gmail.com>
2022-08-28 11:09:06 -0300

    feat:ensure CreateTeam trows CreateTeamError if PlayerRepository findById method returns no player

--- src/aplication/usecases/CreateTeam.ts
+++ src/aplication/usecases/CreateTeam.ts
@@ -1,3 +1,4 @@
+import { CreateTeamError } from '@/aplication/errors/CreateTeamError'
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { UseCase } from '@/aplication/protocols/UseCase'
 
@@ -5,7 +6,11 @@ export class CreateTeam implements UseCase<Input, Output> {
   constructor (private readonly playerRepository: PlayerRepository) {}
 
   async execute (input: Input): Promise<Output> {
-    await this.playerRepository.findById(input.playerId)
+    const player = await this.playerRepository.findById(input.playerId)
+
+    if (player === undefined) {
+      throw new CreateTeamError('player not found')
+    }
     return await Promise.resolve({
       id: 'anyTeamid',
       name: 'anyTeamName',
--- tests/aplication/usecases/CreateTeam.spec.ts
+++ tests/aplication/usecases/CreateTeam.spec.ts
@@ -1,5 +1,7 @@
+import { CreateTeamError } from '@/aplication/errors/CreateTeamError'
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { CreateTeam } from '@/aplication/usecases/CreateTeam'
+import { Player } from '@/domain/entities/Player'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('CreateTeam', () => {
@@ -8,6 +10,16 @@ describe('CreateTeam', () => {
   beforeEach(() => {
     playerRepository = mock<PlayerRepository>()
     sut = new CreateTeam(playerRepository)
+    playerRepository.findById.mockResolvedValue(
+      new Player(
+        'anyId',
+        'anyEmail@gmail.com',
+        'hashedPassword',
+        'anyPixkey',
+        'anyGamerTag',
+        'anyPlatForm'
+      )
+    )
   })
 
   it('should create a new Team', async () => {
@@ -36,5 +48,17 @@ describe('CreateTeam', () => {
     await sut.execute(input)
 
     expect(playerRepository.findById).toHaveBeenCalledWith('anyPlayerId')
+  })
+  it('should throw CreateTeamError if player with given id was not found', async () => {
+    playerRepository.findById.mockResolvedValueOnce(undefined)
+    const input = {
+      playerId: 'anyPlayerId',
+      name: 'anyTeamName',
+      logo: 'anyTeamLogo'
+    }
+
+    await expect(sut.execute(input)).rejects.toThrow(
+      new CreateTeamError('player not found')
+    )
   })
 })
