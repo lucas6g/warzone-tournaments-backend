1b546924b156ddf1ebff25eeafd54c54d5908799
lucas6g <lucas.trabalho1533@gmail.com>
2022-08-30 12:40:28 -0300

    feat: ensure AcceptTeamInvite calls TeamRepository findById method with correct teamId

--- src/aplication/usecases/AcceptTeamInvite.ts
+++ src/aplication/usecases/AcceptTeamInvite.ts
@@ -1,11 +1,13 @@
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { TeamInviteRepository } from '@/aplication/protocols/repositories/TeamInviteRepository'
+import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { UseCase } from '@/aplication/protocols/UseCase'
 
 export class AcceptTeamInvite implements UseCase<Input, Output> {
   constructor (
     private readonly teamInviteRepository: TeamInviteRepository,
-    private readonly playerRepository: PlayerRepository
+    private readonly playerRepository: PlayerRepository,
+    private readonly teamRepository: TeamRepository
   ) {}
 
   async execute (input: Input): Promise<Output> {
@@ -13,6 +15,7 @@ export class AcceptTeamInvite implements UseCase<Input, Output> {
       input.teamInviteId
     )
     await this.playerRepository.findById(String(teamInvite?.getPlayerId()))
+    await this.teamRepository.findById(String(teamInvite?.getTeamId()))
     return await Promise.resolve({ status: 'accepted' })
   }
 }
--- tests/aplication/usecases/AcceptTeamInvite.spec.ts
+++ tests/aplication/usecases/AcceptTeamInvite.spec.ts
@@ -1,5 +1,6 @@
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { TeamInviteRepository } from '@/aplication/protocols/repositories/TeamInviteRepository'
+import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { AcceptTeamInvite } from '@/aplication/usecases/AcceptTeamInvite'
 import { TeamInvite } from '@/domain/entities/TeamInvite'
 
@@ -8,12 +9,18 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('AcceptTeamInvite', () => {
   let teamInviteRepository: MockProxy<TeamInviteRepository>
   let playerRepository: MockProxy<PlayerRepository>
+  let teamRepository: MockProxy<TeamRepository>
   let sut: AcceptTeamInvite
 
   beforeEach(() => {
     teamInviteRepository = mock<TeamInviteRepository>()
     playerRepository = mock<PlayerRepository>()
-    sut = new AcceptTeamInvite(teamInviteRepository, playerRepository)
+    teamRepository = mock<TeamRepository>()
+    sut = new AcceptTeamInvite(
+      teamInviteRepository,
+      playerRepository,
+      teamRepository
+    )
 
     teamInviteRepository.findById.mockResolvedValue(
       new TeamInvite('anyTeamInviteId', 'anyTeamId', 'anyPlayerId')
@@ -48,5 +55,14 @@ describe('AcceptTeamInvite', () => {
     await sut.execute(input)
 
     expect(playerRepository.findById).toHaveBeenCalledWith('anyPlayerId')
+  })
+  it('should call TeamRepository  findById method with correct teamId', async () => {
+    const input = {
+      teamInviteId: 'anyTeamInviteId'
+    }
+
+    await sut.execute(input)
+
+    expect(teamRepository.findById).toHaveBeenCalledWith('anyTeamId')
   })
 })
