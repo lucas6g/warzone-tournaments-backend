15a1623bf895c23f2ebf7c344c1b118b01df48ea
lucas6g <lucas.trabalho1533@gmail.com>
2022-08-29 22:09:19 -0300

    feat: ensure InvitePlayerToTeam calls IDGenerator generate method at least once time

--- src/aplication/usecases/InvitePlayerToTeam.ts
+++ src/aplication/usecases/InvitePlayerToTeam.ts
@@ -1,4 +1,5 @@
 import { InvitePlayerToTeamError } from '@/aplication/errors/InvitePlayerToTeamError'
+import { IDGenerator } from '@/aplication/protocols/IDGenerator'
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { UseCase } from '@/aplication/protocols/UseCase'
@@ -6,7 +7,8 @@ import { UseCase } from '@/aplication/protocols/UseCase'
 export class InvitePlayerToTeam implements UseCase<Input> {
   constructor (
     private readonly teamRepository: TeamRepository,
-    private readonly playerRepository: PlayerRepository
+    private readonly playerRepository: PlayerRepository,
+    private readonly idGenerator: IDGenerator
   ) {}
 
   async execute (input: Input): Promise<void> {
@@ -20,6 +22,8 @@ export class InvitePlayerToTeam implements UseCase<Input> {
         `account with gamertag: ${input.gamertag} and platform: ${input.platform} was not found`
       )
     }
+
+    this.idGenerator.generate()
   }
 }
 
--- tests/aplication/usecases/InvitePlayerToTeam.spec.ts
+++ tests/aplication/usecases/InvitePlayerToTeam.spec.ts
@@ -1,18 +1,23 @@
 import { InvitePlayerToTeamError } from '@/aplication/errors/InvitePlayerToTeamError'
+import { IDGenerator } from '@/aplication/protocols/IDGenerator'
 import { PlayerRepository } from '@/aplication/protocols/repositories/PlayerRepository'
 import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { InvitePlayerToTeam } from '@/aplication/usecases/InvitePlayerToTeam'
 import { Player } from '@/domain/entities/Player'
+
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('InvitePlayerToTeam', () => {
   let teamRepository: MockProxy<TeamRepository>
+  let idGenerator: MockProxy<IDGenerator>
   let playerRepository: MockProxy<PlayerRepository>
   let sut: InvitePlayerToTeam
 
   beforeEach(() => {
     teamRepository = mock<TeamRepository>()
     playerRepository = mock<PlayerRepository>()
+    idGenerator = mock<IDGenerator>()
+
     playerRepository.findByGamertagAndPlatform.mockResolvedValue(
       new Player(
         'anyId',
@@ -23,7 +28,7 @@ describe('InvitePlayerToTeam', () => {
         'anyplatform'
       )
     )
-    sut = new InvitePlayerToTeam(teamRepository, playerRepository)
+    sut = new InvitePlayerToTeam(teamRepository, playerRepository, idGenerator)
   })
 
   it('should invite a player to a team', async () => {
@@ -60,4 +65,23 @@ describe('InvitePlayerToTeam', () => {
       )
     )
   })
+  it('should call IdGenerator generate method ', async () => {
+    const input = {
+      teamId: 'anyTeamId',
+      gamertag: 'anygamertag',
+      platform: 'anyplatform'
+    }
+    await sut.execute(input)
+
+    expect(idGenerator.generate).toHaveBeenCalledTimes(1)
+  })
+  // it('should call TeamInviteRepository save method with TeamInvite instance', async () => {
+  //   const input = {
+  //     teamId: 'anyTeamId',
+  //     gamertag: 'anygamertag',
+  //     platform: 'anyplatform'
+  //   }
+  //   await sut.execute(input)
+  //   expect(teamInviteRepository.save).toHaveBeenCalledWith(new TeamInvite())
+  // })
 })
