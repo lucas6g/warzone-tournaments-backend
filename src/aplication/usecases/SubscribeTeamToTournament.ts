3c7c13ec8b4a7c70745f7ebe788af5108a672594
lucas6g <lucas.trabalho1533@gmail.com>
2022-09-01 12:53:17 -0300

    feat: ensure SubscribeTeamToTournament calls TeamRepository findById method with correct teamId

--- src/aplication/usecases/SubscribeTeamToTournament.ts
+++ src/aplication/usecases/SubscribeTeamToTournament.ts
@@ -1,11 +1,16 @@
+import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { TournamentRepository } from '@/aplication/protocols/repositories/TournamentRepository'
 import { UseCase } from '@/aplication/protocols/UseCase'
 
 export class SubscribeTeamToTournament implements UseCase<Input, output> {
-  constructor (private readonly tournamentRepository: TournamentRepository) {}
+  constructor (
+    private readonly tournamentRepository: TournamentRepository,
+    private readonly teamRepository: TeamRepository
+  ) {}
 
   async execute (input: Input): Promise<output> {
     await this.tournamentRepository.findById(input.tournamentId)
+    await this.teamRepository.findById(input.teamId)
     return await Promise.resolve({ subscriptionPaymentStatus: 'PAID' })
   }
 }
--- tests/aplication/usecases/SubscribeTeamToTournament.spec.ts
+++ tests/aplication/usecases/SubscribeTeamToTournament.spec.ts
@@ -1,14 +1,17 @@
+import { TeamRepository } from '@/aplication/protocols/repositories/TeamRepository'
 import { TournamentRepository } from '@/aplication/protocols/repositories/TournamentRepository'
 import { SubscribeTeamToTournament } from '@/aplication/usecases/SubscribeTeamToTournament'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('SubscribeTeamToTournament', () => {
   let tournamentRepository: MockProxy<TournamentRepository>
+  let teamRepository: MockProxy<TeamRepository>
   let sut: SubscribeTeamToTournament
 
   beforeEach(() => {
     tournamentRepository = mock<TournamentRepository>()
-    sut = new SubscribeTeamToTournament(tournamentRepository)
+    teamRepository = mock<TeamRepository>()
+    sut = new SubscribeTeamToTournament(tournamentRepository, teamRepository)
   })
 
   it('should subscribe a Team to a Tournament', async () => {
@@ -32,5 +35,15 @@ describe('SubscribeTeamToTournament', () => {
     expect(tournamentRepository.findById).toHaveBeenCalledWith(
       input.tournamentId
     )
+  })
+  it('should call TeamRepository findById method with correct teamId', async () => {
+    const input = {
+      teamId: 'anyTeamId',
+      tournamentId: 'anyTournamentId'
+    }
+
+    await sut.execute(input)
+
+    expect(teamRepository.findById).toHaveBeenCalledWith(input.teamId)
   })
 })
